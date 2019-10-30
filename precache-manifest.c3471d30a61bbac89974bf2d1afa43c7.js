77e34287ab20a1cde3cbf6ab76b9b5d3d090ed42
Dorusth <dorusth@hotmail.com>
2019-10-30 12:25:10 +0100

    Updates

---
+++ precache-manifest.c3471d30a61bbac89974bf2d1afa43c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd8a03520794cf35b7792aa05549889",
+    "url": "/frontend-applications/index.html"
+  },
+  {
+    "revision": "888e9b98bffe4026ba44",
+    "url": "/frontend-applications/static/css/main.a2d58a24.chunk.css"
+  },
+  {
+    "revision": "292f51776109f7bf9ed0",
+    "url": "/frontend-applications/static/js/2.790bc61e.chunk.js"
+  },
+  {
+    "revision": "888e9b98bffe4026ba44",
+    "url": "/frontend-applications/static/js/main.f5d5fc2e.chunk.js"
+  },
+  {
+    "revision": "e14b76fde58b0f82f633",
+    "url": "/frontend-applications/static/js/runtime-main.803219c5.js"
+  }
+]);

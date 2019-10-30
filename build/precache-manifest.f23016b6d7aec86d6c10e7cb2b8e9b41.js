dbdc1c2c4de6766080e3bd069187871121a22ec9
Dorusth <dorusth@hotmail.com>
2019-10-30 12:09:13 +0100

    added build

---
+++ build/precache-manifest.f23016b6d7aec86d6c10e7cb2b8e9b41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcfb6a474e3251bae871800423fe264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56e8fd23ecc6cfcc7c70",
+    "url": "/static/css/main.a2d58a24.chunk.css"
+  },
+  {
+    "revision": "292f51776109f7bf9ed0",
+    "url": "/static/js/2.790bc61e.chunk.js"
+  },
+  {
+    "revision": "56e8fd23ecc6cfcc7c70",
+    "url": "/static/js/main.df17cecc.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
